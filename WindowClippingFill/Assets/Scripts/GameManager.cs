b14ac185b99c8556f6be077542622c6bf25d4b74
Marc Brgs <marc.brgs@yahoo.fr>
2022-11-29 20:12:03 +0100

    Fill Rect Englobant slow convexe only

--- WindowClippingFill/Assets/Scripts/GameManager.cs
+++ WindowClippingFill/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -22,6 +23,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject textPolygon;
     [SerializeField] private GameObject textWindow;
     
+    [SerializeField] private Image img;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +39,9 @@ public class GameManager : MonoBehaviour
         Debug.Log("(3,1) à (1,1)(1,-1) " + visible(new Vector3(0f, 1f, 0f), new Vector3(1f, 1f, 0f), new Vector3(1f, -1f, 0f)));
         Debug.Log("(0,1) à (1,-1)(1,1) " + visible(new Vector3(0f, 1f, 0f), new Vector3(1f, -1f, 0f), new Vector3(1f, 1f, 0f)));
         Debug.Log("(3,1) à (1,-1)(1,1) " + visible(new Vector3(0f, 1f, 0f), new Vector3(1f, -1f, 0f), new Vector3(1f, 1f, 0f)));
+        
+        img.gameObject.SetActive(false);
+        clearTexture(img.sprite.texture);
     }
     
     // Update is called once per frame
@@ -59,6 +65,8 @@ public class GameManager : MonoBehaviour
             drawingWindow = true;
             textPolygon.SetActive(false);
             textWindow.SetActive(true);
+            img.gameObject.SetActive(false);
+            clearTexture(img.sprite.texture);
         }
     }
 
@@ -209,6 +217,99 @@ public class GameManager : MonoBehaviour
         return true;
     }
     
+    // Remplissage RectEG
+    public void remplissageRectEG()
+    {
+        // Recover data
+        Vector3[] Poly = new Vector3[lrPolygon.positionCount];
+        lrPolygon.GetPositions(Poly);
+        int nb = 0;
+        Vector2[] rectEG = rectangleEnglobant(Poly);
+        int xmin = (int) rectEG[0].x;
+        int ymin = (int) rectEG[0].y;
+        int xmax = (int) rectEG[1].x;
+        int ymax = (int) rectEG[1].y;
+
+        for (int x = xmin; x < xmax; x++)
+        {
+            for (int y = ymin; y < ymax; y++)
+            {
+                if (interieur(x, y, Poly))
+                {
+                    affichePixel(x, y);
+                    nb++;
+                }
+            }
+        }
+        
+        img.sprite.texture.Apply();
+        img.gameObject.SetActive(true);
+    }
+
+    private void clearTexture(Texture2D tex)
+    {
+        for (int x = 0; x < tex.width; x++)
+        {
+            for (int y = 0; y < tex.height; y++)
+            {
+                tex.SetPixel(x, y, Color.clear);
+            }
+        }
+        tex.Apply();
+    }
+    
+    private Vector2[] rectangleEnglobant(Vector3[] Poly)
+    {
+        int xmin = 1920, xmax = 0, ymin = 1080, ymax = 0;
+        
+        for (int i = 0; i < Poly.Length; i++)
+        {
+            Vector2 polyPixel = new Vector2(worldPosXToPixel(Poly[i].x), worldPosYToPixel(Poly[i].y));
+            Debug.Log("worldpos: " + Poly[i].x + " " + Poly[i].y + ", pixel: " + worldPosXToPixel(Poly[i].x) + " " + worldPosYToPixel(Poly[i].y));
+            if (polyPixel.x < xmin)
+              xmin = (int) polyPixel.x;
+            if (polyPixel.x > xmax)
+                xmax = (int) polyPixel.x;
+            if (polyPixel.y < ymin)
+                ymin = (int) polyPixel.y;
+            if (polyPixel.y > ymax)
+                ymax = (int) polyPixel.y;
+        }
+        
+        Debug.Log("xmin: " + xmin + ", xmax: " + xmax + ", ymin: " + ymin + ", ymax: " + ymax);
+        Vector2[] rectEG = new Vector2[2];
+        rectEG[0] = new Vector2(xmin, ymin); // P1
+        rectEG[1] = new Vector2(xmax, ymax); // P2
+        return rectEG;
+    }
+
+    private bool interieur(int x, int y, Vector3[] poly)
+    {
+        // visible pour chaque côté
+        for (int i = 0; i < poly.Length-1; i++)
+        {
+            if(!visible(new Vector3(x, y), new Vector3(worldPosXToPixel(poly[i].x), worldPosYToPixel(poly[i].y)), new Vector3(worldPosXToPixel(poly[i+1].x), worldPosYToPixel(poly[i+1].y)))) 
+                return false;
+        }
+        
+        return true;
+    }
+
+    private void affichePixel(int x, int y)
+    {
+        img.sprite.texture.SetPixel(x, y, Color.yellow);
+    }
+    
+    private int worldPosXToPixel(float v)
+    {
+        return (int) (((v + 5.33) * 1920) / 10.66);
+    }
+    
+    private int worldPosYToPixel(float v)
+    {
+        return (int) (((v + 3) * 1080) / 6);
+    }
+    
     private void drawWindow()
     {
         if (Input.GetMouseButtonDown(0))
@@ -243,13 +344,34 @@ public class GameManager : MonoBehaviour
             polygonIndex++;
         }
 
-        if (lrPolygon.positionCount != 0 && Input.GetMouseButtonDown(1))
+        if (lrPolygon.positionCount > 1 && Input.GetMouseButtonDown(1))
         {
             lrPolygon.positionCount = polygonIndex + 1;
             lrPolygon.SetPosition(polygonIndex, lrPolygon.GetPosition(0));
             polygonIndex++;
             drawingPolygon = false;
             textPolygon.SetActive(false);
+        }
+    }
+    
+    public static void DrawLine(Vector2 p1, Vector2 p2, Color col)
+    {
+        Vector2 t = p1;
+        float frac = 1/Mathf.Sqrt (Mathf.Pow (p2.x - p1.x, 2) + Mathf.Pow (p2.y - p1.y, 2));
+        float ctr = 0;
+     
+        while ((int)t.x != (int)p2.x || (int)t.y != (int)p2.y) {
+            t = Vector2.Lerp(p1, p2, ctr);
+            ctr += frac;
+            // img.sprite.texture.SetPixel((int)t.x, (int)t.y, col);
+        }
+    }
+
+    public void drawShape()
+    {
+        for (int i = 0; i < lrPolygon.positionCount - 1; i++)
+        {
+            DrawLine( lrPolygon.GetPosition(i), lrPolygon.GetPosition(i+1), Color.blue);
         }
     }
 }
